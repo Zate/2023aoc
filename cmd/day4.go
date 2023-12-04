260c3d39482d9cdbdecd39a1c13293714b26a20b
Zate <zate75@gmail.com>
2023-12-04 21:21:34 +0800

    day4 more.

--- cmd/day4.go
+++ cmd/day4.go
@@ -4,25 +4,45 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+type card struct {
+	id          int
+	winners     []int
+	myNums      []int
+	winningNums []int
+	points      int
+}
+
+type scratchGame struct {
+	cards       []card
+	totalPoints int
+}
+
 // day4Cmd represents the day4 command
-var day4Cmd = &cobra.Command{
-	Use:   "day4",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+var (
+	day4Cmd = &cobra.Command{
+		Use:   "day4",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		day4(cmd, args)
-	},
-}
+		Run: func(cmd *cobra.Command, args []string) {
+			day4(cmd, args)
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(day4Cmd)
@@ -41,4 +61,76 @@ func init() {
 
 func day4(cmd *cobra.Command, args []string) {
 	fmt.Println("day4 called")
+	var Game scratchGame
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	Game.totalPoints = 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// find the first and last numbers in each line
+		Game.cards = append(Game.cards, processCard(line))
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	for card := range Game.cards {
+		Game.totalPoints += Game.cards[card].points
+	}
+	fmt.Printf("Total: %d\n", Game.totalPoints)
+}
+
+func processCard(line string) card {
+	var c card
+	cardArray := strings.Split(line, ":")
+	idArray := strings.Split(cardArray[0], " ")
+	id, err := strconv.Atoi(idArray[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	c.id = id
+	numArray := strings.Split(cardArray[1], "|")
+	winnersArray := strings.Split(numArray[0], " ")
+	myNumbersArray := strings.Split(numArray[1], " ")
+	c.myNums = make([]int, len(myNumbersArray))
+	for i, num := range myNumbersArray {
+		c.myNums[i], err = strconv.Atoi(num)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	c.winners = make([]int, len(winnersArray))
+	for i, num := range winnersArray {
+		c.winners[i], err = strconv.Atoi(num)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	c.winningNums = make([]int, len(c.winners))
+	for _, winner := range c.winners {
+		for _, num := range c.myNums {
+			if winner == num {
+				c.winningNums = append(c.winningNums, winner)
+			}
+		}
+	}
+	points := 1
+	for i := 0; i < len(c.winningNums); i++ {
+		points *= 2
+	}
+	c.points = points
+
+	return c
 }
--- cmd/day4_test.go
+++ cmd/day4_test.go
@@ -4,9 +4,19 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	testData4 = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+	Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+	Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+	Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+	Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+	Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 )
 
 func Test_day4(t *testing.T) {
@@ -23,6 +33,43 @@ func Test_day4(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			day4(tt.args.cmd, tt.args.args)
+		})
+	}
+}
+
+func Test_processCard(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want card
+	}{
+		{
+			name: "Card 1",
+			args: args{
+				line: "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+			},
+			// id          int
+			// winners     []int
+			// myNums      []int
+			// winningNums []int
+			// points      int
+			want: card{
+				id:          1,
+				winners:     []int{41, 48, 83, 86, 17},
+				myNums:      []int{83, 86, 6, 31, 17, 9, 48, 53},
+				winningNums: []int{48, 83, 86, 17},
+				points:      8,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := processCard(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("processCard() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
